0dc5ad67bb9ec842356fd6106501c46398ea6159
Gabriela Díaz <gldt2494@gmail.com>
2024-11-05 18:18:43 -0500

    feat: implementa csurf

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const port=3000;
-const MONGODB_URI='mongodb+srv://santaaparicioc:typing1234@cluster0.9o6gj.mongodb.net/samsung?retryWrites=true&w=majority'
+const port = 3000;
+const MONGODB_URI = 'mongodb+srv://santaaparicioc:typing1234@cluster0.9o6gj.mongodb.net/samsung?retryWrites=true&w=majority'
 
 const mongoose = require('mongoose');
 const session = require('express-session');
@@ -13,6 +13,7 @@ const raizDir = require('./utils/path');
 
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
+const csrf = require('csurf');
 
 //from project
 const usuarioRouter = require('./routes/usuario')
@@ -27,9 +28,11 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 
+const csrfProtection = csrf();
+
 app.get("/favicon.ico", function (req, res) {
-    res.sendStatus(204);
-  });
+  res.sendStatus(204);
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -39,6 +42,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(raizDir, 'public')));
 app.use(session({ secret: 'algo muy secreto', resave: false, saveUninitialized: false, store: store }));
 app.use(flash());
+app.use(csrfProtection());
 
 
 app.use((req, res, next) => {
@@ -48,9 +52,9 @@ app.use((req, res, next) => {
   }
   Usuario.findById(req.session.usuario._id)
     .then(usuario => {
-        req.usuario = usuario;
-        res.locals.user = usuario;
-        next();
+      req.usuario = usuario;
+      res.locals.user = usuario;
+      next();
     })
     .catch(err => console.log(err));
 
@@ -58,24 +62,24 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.locals.autenticado = req.session.autenticado;
-  // res.locals.csrfToken = req.csrfToken();
+  res.locals.csrfToken = req.csrfToken();
   next();
 });
 
 app.use('/admin', adminRouter);
-app.use('/usuario',usuarioRouter)
+app.use('/usuario', usuarioRouter)
 app.use(ecommerceRouter);
 
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(raizDir, 'views', '404.ejs'));
+  res.status(404).sendFile(path.join(raizDir, 'views', '404.ejs'));
 })
 
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-   
-    app.listen(port,(e)=>{console.log(`...running port ${port}`)});
+
+    app.listen(port, (e) => { console.log(`...running port ${port}`) });
   })
   .catch(err => {
     console.log(err);

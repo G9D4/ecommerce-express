fe7b93e3ad5f297a1ed8b389206dbcd44876a1b2
melsyhuamani <melsyhuamanivargas@gmail.com>
2024-11-16 20:35:20 -0500

    feat: validaciones en routes para editar y crear producto

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, body } = require('express-validator');
 
 const router = express.Router();
 
@@ -7,14 +8,50 @@ const adminController = require('../controllers/admin')
 const isAuth = require('../middleware/is-auth');
 
 router.get('/crear-producto', isAuth, adminController.getCrearProducto);
-router.post('/crear-producto', isAuth, adminController.postCrearProducto);
-
 router.get('/productos', isAuth, adminController.getProductos);
-
-
-// Cambia la ruta de editar producto para incluir el ID del producto
 router.get('/editar-producto/:id', isAuth, adminController.getEditProductos);
-router.post('/editar-producto', isAuth, adminController.postEditProductos);
+
+router.post('/crear-producto',
+    [
+        body('nombreproducto', 'El nombre del producto debe ser un texto de no menos de 3 caracteres')
+            .trim()
+            .isString()
+            .isLength({ min: 3 }),
+        body('urlImagen', 'Ingrese un URL válido')
+            .isURL(),
+        body('precio', 'El precio debe ser un número')
+            .isFloat(),
+        body('descripcion', 'La descripción debe ser un texto de entre 10 a 400 caracteres')
+            .trim()
+            .isLength({ min: 10, max: 400 }),
+        body('caracteristicas', 'Las caracteristicas deben estar entre 10 a 300 caracteres')
+            .trim()
+            .isString()
+            .isLength({ min: 10, max: 300 }),
+    ],
+    isAuth,
+    adminController.postCrearProducto);
+
+router.post('/editar-producto/:id',
+    [
+        body('nombreproducto', 'El nombre del producto debe ser un texto de no menos de 3 caracteres')
+            .trim()
+            .isString()
+            .isLength({ min: 3 }),
+        body('urlImagen', 'Ingrese un URL válido')
+            .isURL(),
+        body('precio', 'El precio debe ser un número')
+            .isFloat(),
+        body('descripcion', 'La descripción debe ser un texto de entre 10 a 400 caracteres')
+            .trim()
+            .isLength({ min: 10, max: 400 }),
+        body('caracteristicas', 'Las caracteristicas deben estar entre 10 a 300 caracteres')
+            .trim()
+            .isString()
+            .isLength({ min: 10, max: 300 }),
+    ],
+    isAuth,
+    adminController.postEditProductos);
 router.post('/eliminar-producto', isAuth, adminController.postEliminarProducto);
 
 

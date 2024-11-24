e26db168f87c5b9d4d0455ab69272b6ed27d1c36
melsyhuamani <melsyhuamanivargas@gmail.com>
2024-11-23 20:29:16 -0500

    feat: controller y routers para accesibilidad, privacidad y legal

--- controllers/empresa.js
+++ controllers/empresa.js
@@ -30,3 +30,36 @@ exports.getCondicionesCompra = async (req, res, next) => {
         console.log(error);
     }
 };
+
+exports.getAccesibilidad = async (req, res, next) => {
+    try {
+        res.render("empresa/accesibilidad", {
+            titulo: "Ayuda para Accesibilidad",
+            path: "/accesibilidad"
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.getPrivacidad = async (req, res, next) => {
+    try {
+        res.render("empresa/privacidad", {
+            titulo: "Privacidad",
+            path: "/privacidad"
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.getLegal = async (req, res, next) => {
+    try {
+        res.render("empresa/legal", {
+            titulo: "Legal",
+            path: "/legal"
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
--- routes/empresa.js
+++ routes/empresa.js
@@ -8,5 +8,8 @@ const isAuth = require('../middleware/is-auth')
 router.get('/nosotros', isAuth, empresaController.getNosotros);
 router.get('/faqs', isAuth, empresaController.getSoporte);
 router.get('/condiciones-compra', isAuth, empresaController.getCondicionesCompra);
+router.get('/accesibilidad', isAuth, empresaController.getAccesibilidad);
+router.get('/privacidad', isAuth, empresaController.getPrivacidad);
+router.get('/legal', isAuth, empresaController.getLegal);
 
 module.exports = router;
